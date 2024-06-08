9910d454068c0380f3f14a171129663c26f171d9
skatla <sanjaykatla.sanjay@gmail.com>
2024-06-08 22:53:54 +0700

    Git init DataSyncSchedulerApplication

---
+++ src/main/java/org/sanjay/datasyncscheduler/DataSyncSchedulerApplication.java
@@ -0,0 +1,13 @@
+package org.sanjay.datasyncscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSyncSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataSyncSchedulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sanjay/datasyncscheduler/DataSyncSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sanjay.datasyncscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSyncSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
