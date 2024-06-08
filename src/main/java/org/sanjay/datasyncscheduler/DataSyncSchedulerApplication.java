c626166ec0623ea142db1d3b25acd85636364917
skatla <sanjaykatla.sanjay@gmail.com>
2024-06-09 00:03:19 +0700

    test logging

--- src/main/java/org/sanjay/datasyncscheduler/DataSyncSchedulerApplication.java
+++ src/main/java/org/sanjay/datasyncscheduler/DataSyncSchedulerApplication.java
@@ -1,13 +1,18 @@
 package org.sanjay.datasyncscheduler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DataSyncSchedulerApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(DataSyncSchedulerApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(DataSyncSchedulerApplication.class, args);
+        logger.info("Data Sync Scheduler Application Started");
+        logger.error("Data Sync Scheduler Application Started");
     }
 
 }
